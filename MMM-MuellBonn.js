ac06f5e4b3c807f0caca1f4f509fd9430cfa8ee8
schornhe <49951046+schornhe@users.noreply.github.com>
2024-04-03 11:23:57 +0200

    log ergänzt

--- MMM-MuellBonn.js
+++ MMM-MuellBonn.js
@@ -5,39 +5,42 @@
  * MIT Licensed.
  */
 Module.register("MMM-MuellBonn", {
-  // Default module config
-  defaults: {
-      daysFromToday: 4,
-      csvFilePath: 'modules/MMM-MuellBonn/trash_collection_schedule.csv', // Default CSV file path
-      iconPath: 'modules/MMM-MuellBonn/icons/',
-      updateInterval: 60 * 60 * 1000, // Update interval in milliseconds (1 hour in this example)
-  },
+    // Default module config
+    defaults: {
+        daysFromToday: 4,
+        csvFilePath: 'modules/MMM-MuellBonn/trash_collection_schedule.csv', // Default CSV file path
+        iconPath: 'modules/MMM-MuellBonn/icons/',
+        updateInterval: 60 * 60 * 1000, // Update interval in milliseconds (1 hour in this example)
+    },
+  
+    // Override socket notification handler
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'TRASH_SCHEDULE_LOADED') {
+            this.processTrashSchedule(payload.trashSchedule);
+        } else if (notification === 'TRASH_SCHEDULE_ERROR') {
+            console.error('Error loading trash schedule:', payload.error);
+        }
+    },
+  
+    // Override start method
+    start: function() {
+      console.log("MMM-MuellBonn module started!");
+  
+      // Request trash schedule from node helper
+      this.sendSocketNotification('GET_TRASH_SCHEDULE', { csvFilePath: this.config.csvFilePath });
+ 
+      console.log("MMM-MuellBonn get trash schedule!");
 
-  console.log("MMM-MuellBonn defaults gesetzt");
+      // Schedule updates
+      setInterval(() => {
+          this.sendSocketNotification('GET_TRASH_SCHEDULE', { csvFilePath: this.config.csvFilePath });
+      }, this.config.updateInterval);
 
-  // Override socket notification handler
-  socketNotificationReceived: function(notification, payload) {
-      if (notification === 'TRASH_SCHEDULE_LOADED') {
-          this.processTrashSchedule(payload.trashSchedule);
-      } else if (notification === 'TRASH_SCHEDULE_ERROR') {
-          console.error('Error loading trash schedule:', payload.error);
-      }
-  },
+       console.log("MMM-MuellBonn set update");
 
-  console.log("MMM-MuellBonn vor Aufruf Startfiunktion");
-
-  // Override start method
-  start: function() {
-    console.log("MMM-MuellBonn module started!");
-
-    // Request trash schedule from node helper
-    this.sendSocketNotification('GET_TRASH_SCHEDULE', { csvFilePath: this.config.csvFilePath });
-
-    // Schedule updates
-    setInterval(() => {
-        this.sendSocketNotification('GET_TRASH_SCHEDULE', { csvFilePath: this.config.csvFilePath });
-    }, this.config.updateInterval);
-},
+    }
+  });
+  
 
   // Process loaded trash schedule
   processTrashSchedule: function(trashSchedule) {
